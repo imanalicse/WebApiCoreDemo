96b4c66b8f6ed6f5d7043c7d67b08a86dfc6a439
iman <iman@bitmascot.com>
2018-07-24 11:36:40 +0600

    Initial crud

---
+++ WebApiCoreDemo/Controllers/TicketController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCoreDemo.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TicketController : Controller
+    {
+        private TicketContext _context;
+
+        public TicketController(TicketContext context)
+        {
+            _context = context;
+
+            if (_context.TicketItems.Count() == 0)
+            {
+                _context.TicketItems.Add(new TicketItem { Concert = "Beyonce" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public IEnumerable<TicketItem> GetAll()
+        {
+            return _context.TicketItems.AsNoTracking().ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetTicket")]
+        public IActionResult GetById(long id)
+        {
+            var ticket = _context.TicketItems.FirstOrDefault(t => t.Id == id);
+
+            if (ticket == null)
+            {
+                return NotFound(); // 404                              
+            }
+
+            return new ObjectResult(ticket);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]TicketItem ticket)
+        {
+            if (ticket == null)
+            {
+                return BadRequest();
+            }
+            _context.TicketItems.Add(ticket);
+            _context.SaveChanges();
+
+            //Return "/Ticket/" + ticket.Id
+            return CreatedAtRoute("GetTicket", new { id = ticket.Id }, ticket);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(long id, [FromBody] TicketItem ticket)
+        {
+            if (ticket == null || ticket.Id != id)
+            {
+                return BadRequest();
+            }
+
+            var tic = _context.TicketItems.FirstOrDefault(t => t.Id == id);
+            if (tic == null)
+            {
+                return NotFound();
+            }
+
+            tic.Concert = ticket.Concert;
+            tic.Artist = ticket.Artist;
+            tic.Available = ticket.Available;
+
+            _context.TicketItems.Update(tic);
+            _context.SaveChanges();
+
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var tic = _context.TicketItems.FirstOrDefault(t => t.Id == id);
+            if (tic == null)
+            {
+                return NotFound();
+            }
+            _context.TicketItems.Remove(tic);
+            _context.SaveChanges();
+            return new NoContentResult();
+        }
+    }
+}
---
+++ WebApiCoreDemo/TicketItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiCoreDemo
+{
+    public class TicketItem
+    {
+        public long Id { get; set; }
+        public string Concert { get; set; }
+        public string Artist { get; set; }
+        public bool Available { get; set; }
+    }
+
+    public class TicketContext : DbContext
+    {
+        public TicketContext(DbContextOptions<TicketContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TicketItem> TicketItems { get; set; }
+    }
+}
